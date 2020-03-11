5b8b3ab694664148787ee994f10d9ffae7d236b0
Joshua Fehler <jsfehler@gmail.com>
2020-03-10 22:22:30 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'chalice',
+]
+
+setup(
+    name='leangle',
+    version='0.0.1',
+    description='Add response descriptions to chalice',
+    author='Joshua Fehler',
+    url='https://github.com/jsfehler/leangle',
+    install_requires=install_requires,
+    license='GNU General Public License v3.0',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
