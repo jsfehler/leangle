16dcf97175606b38d4268b912eebdd1c31059c11
Joshua Fehler <jsfehler@gmail.com>
2020-03-12 09:43:48 -0400

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 
 setup(
     name='leangle',
-    version='0.0.11',
+    version='0.1.0',
     description='Add response descriptions to chalice',
     long_description=read('README.rst'),
     author='Joshua Fehler',
