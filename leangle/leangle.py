0f3cd1bb44dd021dc480ed3afa9fcaf33394b5a2
Joshua Fehler <jsfehler@gmail.com>
2020-03-16 00:42:55 -0400

    Ignore ImportError on chalice.deploy

--- leangle/leangle.py
+++ leangle/leangle.py
@@ -1,15 +1,19 @@
 from typing import Callable
 
-from chalice.deploy.swagger import SwaggerGenerator
-
 
 _leangle_schemas = []
 
 # Patches
-from .chalice_patches import patch_generate_route_method, patch_generate_swagger  # NOQA
+# On AWS, chalice does not include deploy, so ignore patching it.
+try:
+    from .chalice_patches import patch_generate_route_method, patch_generate_swagger  # NOQA
+    from chalice.deploy.swagger import SwaggerGenerator # NOQA
 
-SwaggerGenerator.generate_swagger = patch_generate_swagger()
-SwaggerGenerator._generate_route_method = patch_generate_route_method()
+    SwaggerGenerator.generate_swagger = patch_generate_swagger()
+    SwaggerGenerator._generate_route_method = patch_generate_route_method()
+
+except ImportError:
+    pass
 
 
 def add_schema() -> Callable:
